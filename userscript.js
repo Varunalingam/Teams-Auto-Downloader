5683494fae04ea0e2cc7aef8076d2b457ac2f400
Varunalingam <varunalingam.v@gmail.com>
2021-02-19 11:50:33 +0530

    fix : script name and remove wait fn

--- userscript.js
+++ userscript.js
@@ -11,14 +11,8 @@
 (function() {
     'use strict';
     console.log("Auto Download Script is Initiated")
-    var script = document.createElement('script'); 
+    var script = document.createElement('script-auto-download'); 
     script.src = 'https://raw.githubusercontent.com/Varunalingam/teams-auto-download/master/AutoDownload.js';
     script.type = 'text/javascript'; 
     document.getElementsByTagName('body')[0].appendChild(script); 
-    function wait() { 
-        if(typeof unsafeWindow.jQuery == 'undefined') 
-    { window.setTimeout(GM_wait,100); } 
-            else { $ = unsafeWindow.jQuery; console.log("Auto Download is Loaded") } 
-    } 
-    wait(); 
 })();
